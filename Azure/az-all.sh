96618408f3cdcd09a3005446a0fdae13e96da5ea
Wilson Mar <wilsonmar+github@gmail.com>
2021-04-13 23:13:25 -0600

    Add network watcher

--- Azure/az-all.sh
+++ Azure/az-all.sh
@@ -5,5 +5,19 @@
 # cd to folder, copy this line and paste in the terminal:
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/Azure/az-all.sh)" -v -i
 
+# Implements Lab https://github.com/MicrosoftLearning/AZ-303-Microsoft-Azure-Architect-Technologies/blob/master/Instructions/Labs/Module_05_Lab.md
+
 # No need for az login within Cloud Shell.
 echo "STEP 00 - Login Azure:"
+
+# QUESTION: Why is this necessary?
+# az provider show -n Microsoft.Insights
+   # In Json response: 
+   # az provider register --namespace 'Microsoft.Insights'
+
+LOCATION='uswest2'
+   # az account list-locations --query "[].{name:name}" -o table
+MY_RG="NetworkWatcherRG"  # per lab   
+   
+echo "*** Add network watcher"
+az network watcher configure --resource-group $MY_RG --locations $LOCATION --enabled -o table
