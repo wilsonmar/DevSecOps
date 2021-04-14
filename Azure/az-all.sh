d72484b3ebb392952a5e0d06fae8562ea5b0aa9e
Wilson Mar <wilsonmar+github@gmail.com>
2021-04-13 23:06:47 -0600

    new echo

---
+++ Azure/az-all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# az-all.sh
+
+# After you obtain a Terminal (console) in your environment,
+# cd to folder, copy this line and paste in the terminal:
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/Azure/az-all.sh)" -v -i
+
+# No need for az login within Cloud Shell.
+echo "STEP 00 - Login Azure:"
