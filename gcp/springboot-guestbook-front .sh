9073572a587b4765e816fe1867b5619a6c2f5cdd
Wilson Mar <wilsonmar+github@gmail.com>
2021-07-14 21:01:24 -0600

    Update springboot-guestbook-front .sh

--- gcp/springboot-guestbook-front .sh
+++ gcp/springboot-guestbook-front .sh
@@ -8,23 +8,24 @@
 # Highlight this command, copy it and paste it in the Google Cloud Shell within Qwiklabs:
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/gcp/springboot-guestbook-front.sh)"
 
-uname -a
+# Host info: uname -a
 
 if [ -z "${MY_REGION+x}" ]; then
    MY_REGION="us-central1"
 fi
 echo ">>> MY_REGION=$MY_REGION"
-
 gcloud config set compute/zone "$MY_REGION"
 gcloud config list
 
-echo ">>> Task 1. Bootstrap the application"
+echo ">>> Run the backend"
 cd ~/
 git clone https://github.com/saturnism/spring-cloud-gcp-guestbook.git
 
+echo ">>> Make a copy of the initial version of the backend application (guestbook-service)"
 cp -a ~/spring-cloud-gcp-guestbook/1-bootstrap/guestbook-service \
   ~/guestbook-service
 
+echo ">>> Run the backend application"
 cd ~/guestbook-service
 ./mvnw -q spring-boot:run -Dserver.port=8081  
 
