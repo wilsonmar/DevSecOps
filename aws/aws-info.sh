a2a1a2b4e5967a7dae2260cc903630d01580b673
wilsonmar <wilsonmar+github@gmail.com>
2021-09-13 15:31:20 -0600

    Update

--- aws/aws-info.sh
+++ aws/aws-info.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+l#!/usr/bin/env bash
 
-# aws-info.sh in DevSecOps/ aws
+# aws-info.sh in https://github.com/wilsonmar/DevSecOps/blob/main/aws/aws-info.sh
 # Based on https://medium.com/circuitpeople/aws-cli-with-jq-and-bash-9d54e2eabaf1
 #          https://theagileadmin.com/2017/05/26/aws-cli-queries-and-jq/
 
@@ -9,10 +9,10 @@
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/aws/aws-info.sh)"
 
 
-OS_TYPE="macOS"
+export OS_TYPE="macOS"
+export AWS_PROFILE="cdb-aws-09"
 
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html
-export AWS_PROFILE="cdb-aws-09"
 echo ">>> Edit ~/.aws/credentials"
 echo ">>> Setup credentials from AVM to AWS_PROFILE=$AWS_PROFILE :"
 
@@ -28,8 +28,6 @@ fi
 # echo ">>> When was AWS user account created?"
 # https://docs.aws.amazon.com/cli/latest/reference/iam/get-user.html
 # ERROR: aws iam get-user --user-name "$AWS_PROFILE" --cli-input-json "json" | jq -r ".User.CreateDate[:4]" 
-
-# Laghima Mishra on golden images
 
 echo ">>> What AWS account is being used?"
 { aws sts get-caller-identity & aws iam list-account-aliases; } | jq -s ".|add"
@@ -68,21 +66,64 @@ aws lambda list-functions | jq -r '[.Functions[]|{name: .FunctionName, env: .Env
 echo ">>> RDS (Relational Data Service) Instance Endpoints:"
 aws rds describe-db-instances | jq -r '.DBInstances[] | { (.DBInstanceIdentifier):(.Endpoint.Address + ":" + (.Endpoint.Port|tostring))}'
 
-echo ">>> How many AWS services?"
+echo ">>> How many AWS services available?"
 curl -s https://raw.githubusercontent.com/boto/botocore/develop/botocore/data/endpoints.json \
    | jq -r '.partitions[0].services | keys[]' | wc -l
 
+# While it *can* be answered in the Config console UI (given enough clicks), 
+# or using Cost Explorer (fewer clicks), 
+if [ "$OS_TYPE" == "macOS" ]; then  # it's on a Mac:
+   # For MacOS: https://stackoverflow.com/questions/63559669/get-first-date-of-current-month-in-macos-terminal
+   # And https://www.freebsd.org/cgi/man.cgi?date
+   MONTH_FIRST_DAY=$( date -v1d -v-1m '+%Y-%m-%d' )  # yields 2021-08-01 previous month start
+   # MONTH_FIRST_DAY=$( date -v1d -v"$(date '+%m')"m '+%Y-%m-%d' )  # yields 2021-09-01
+      # The -v1d is a time adjust flag to move to the first day of the month
+      # In -v"$(date '+%m')"m, we get the current month number using date '+%m' and use it to populate the month adjust field. So e.g. for Aug 2020, its set to -v8m
+      # The '+%F' prints the date in YYYY-MM-DD format. If not supported in your date version, use +%Y-%m-%d explicitly.
+      # To print all 12 months: for mon in {1..12}; do; date -v1d -v"$mon"m '+%F'; done
+   # MONTH_LAST_DAY=$( date -v1d -v-1d -v+1m +%Y-%m-%d )  # for 2021-09-30
+   MONTH_LAST_DAY=$( date -v1d -v-1d -v+0m +%Y-%m-%d )  # for 2021-09-30
+
+else
+   # This uses GNU date on Linus: not portable (notably Mac / *BSD date is different)
+   # https://unix.stackexchange.com/questions/223543/get-the-date-of-last-months-last-day-in-a-shell-script
+   MONTH_FIRST_DAY=$( date "+%Y-%m-01" -d "-1 Month" )
+   MONTH_LAST_DAY=$( date --date="$(date +'%Y-%m-01') - 1 second" -I )  # for 2021-09-30
+fi
+   echo ">>> From $OS_TYPE - $MONTH_FIRST_DAY to $MONTH_LAST_DAY "
+   # See https://docs.aws.amazon.com/cli/latest/reference/ce/get-cost-and-usage.html
+#   aws ce get-cost-and-usage --time-period Start="$MONTH_FIRST_DAY",End="$MONTH_LAST_DAY" \
+#      --granularity MONTHLY --metrics UsageQuantity \
+#      --group-by Type=DIMENSION,Key=SERVICE | jq '.ResultsByTime[].Groups[] | select(.Metrics.UsageQuantity.Amount > 0) | .Keys[0]'
+      # date "+%Y-%m-01" yields 2021-09-01, see https://www.cyberciti.biz/faq/linux-unix-formatting-dates-for-display/
+      # See https://stackoverflow.com/questions/27920201/how-can-i-get-the-1st-and-last-date-of-the-previous-month-in-a-bash-script/46897063
+
+   echo ">>> What is each service costing me for the previous month:"
+   aws ce get-cost-and-usage --time-period Start="$MONTH_FIRST_DAY",End="$MONTH_LAST_DAY" \
+      --granularity MONTHLY --metrics USAGE_QUANTITY BLENDED_COST  \
+      --group-by Type=DIMENSION,Key=SERVICE | jq '[ .ResultsByTime[].Groups[] | select(.Metrics.BlendedCost.Amount > "0") | { (.Keys[0]): .Metrics.BlendedCost } ] | sort_by(.Amount) | add'
+
+   echo ">>> What is each service costing me for the current month:"
+   aws ce get-cost-and-usage --time-period Start="$MONTH_FIRST_DAY",End="$MONTH_LAST_DAY" \
+      --granularity MONTHLY --metrics USAGE_QUANTITY BLENDED_COST  \
+      --group-by Type=DIMENSION,Key=SERVICE | jq '[ .ResultsByTime[].Groups[] | select(.Metrics.BlendedCost.Amount > "0") | { (.Keys[0]): .Metrics.BlendedCost } ] | sort_by(.Amount) | add'
+
+
 echo ">>> EC2 parentage: Which EC2 Instances were created by Stacks?"
-for stack in $(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE UPDATE_COMPLETE | jq -r '.StackSummaries[].StackName'); do aws cloudformation describe-stack-resources --stack-name $stack | jq -r '.StackResources[] | select (.ResourceType=="AWS::EC2::Instance")|.PhysicalResourceId'; done;
+for stack in $(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE UPDATE_COMPLETE \
+   | jq -r '.StackSummaries[].StackName'); do aws cloudformation describe-stack-resources --stack-name $stack \
+   | jq -r '.StackResources[] | select (.ResourceType=="AWS::EC2::Instance")|.PhysicalResourceId'; done;
 
 echo ">>> How many Snapshot volumes do I have?"
 aws ec2 describe-snapshots --owner-ids self | jq '.Snapshots | length'
+   # 4
 
 echo ">>> how large are EC2 Snapshots in total?"
 aws ec2 describe-snapshots --owner-ids self | jq '[.Snapshots[].VolumeSize] | add'
 
-echo ">>> H do they breakdown by the volume used to create them?"
-aws ec2 describe-snapshots --owner-ids self | jq '.Snapshots | [ group_by(.VolumeId)[] | { (.[0].VolumeId): { "count": (.[] | length), "size": ([.[].VolumeSize] | add) } } ] | add'
+echo ">>> How do Snapshots breakdown by the volume used to create them?"
+aws ec2 describe-snapshots --owner-ids self \
+   | jq '.Snapshots | [ group_by(.VolumeId)[] | { (.[0].VolumeId): { "count": (.[] | length), "size": ([.[].VolumeSize] | add) } } ] | add'
 
 
 echo ">>> getting the log group names (space delimited):"
@@ -95,17 +136,18 @@ for group in $logs; do echo $(aws logs describe-log-streams --log-group-name $gr
 exit -1
 
 
-echo ">>> Loop through the groups and streams and get the last 10 messages since midnight:"
+# echo ">>> Loop through the groups and streams and get the last 10 messages since midnight:"
 # for group in $logs; do for stream in $(aws logs describe-log-streams --log-group-name $group --order-by LastEventTime --descending --max-items 1 | jq -r '[ .logStreams[0].logStreamName + " "] | add'); do echo ">>>"; echo GROUP: $group; echo STREAM: $stream; aws logs get-log-events --limit 10 --log-group-name $group --log-stream-name $stream --start-time $(date -d 'today 00:00:00' '+%s%N' | cut -b1-13) | jq -r ".events[].message"; done; done
 
 
-echo ">>> Much Data is in Each of my Buckets?"
+echo ">>> How much Data is in Each of my S3 Buckets?"
    # CloudWatch contains the data, but if your account has more than a few buckets it’s very tedious to use.
    # This little command gives your the total size of the objects in each bucket, one per line, with human-friendly numbers:
 for bucket in $( aws s3api list-buckets --query "Buckets[].Name" --output text); \
-   do aws cloudwatch get-metric-statistics --namespace AWS/S3 --metric-name BucketSizeBytes --dimensions Name=BucketName,Value=$bucket Name=StorageType,Value=StandardStorage --start-time $(date --iso-8601)T00:00 --end-time $(date --iso-8601)T23:59 --period 86400 --statistic Maximum | echo $bucket: $(numfmt --to si $(jq -r ".Datapoints[0].Maximum // 0")); done;
+   do aws cloudwatch get-metric-statistics --namespace AWS/S3 --metric-name BucketSizeBytes --dimensions Name=BucketName,Value=$bucket Name=StorageType,Value=StandardStorage --start-time $(date --iso-8601)T00:00 --end-time $(date --iso-8601)T23:59 --period 86400 --statistic Maximum \
+   | echo $bucket: $(numfmt --to si $(jq -r ".Datapoints[0].Maximum // 0")); done;
 
-echo ">>> Prefer to have that is dollars per month? Just a little math ( based on the current standard tier price of $0.023 per GB per month):"
+echo ">>> In dollars per month? (based on the standard tier price of $0.023 per GB per month):"
 for bucket in $(aws s3api list-buckets --query "Buckets[].Name" --output text); do aws cloudwatch get-metric-statistics --namespace AWS/S3 --metric-name BucketSizeBytes --dimensions Name=BucketName,Value=$bucket Name=StorageType,Value=StandardStorage --start-time $(date --iso-8601)T00:00 --end-time $(date --iso-8601)T23:59 --period 86400 --statistic Maximum | echo $bucket: \$$(jq -r "(.Datapoints[0].Maximum //
  0) * .023 / (1024*1024*1024) * 100.0 | floor / 100.0"); done;
 
@@ -141,32 +183,6 @@ export INSTANCE_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --ou
 
 ------------------------
 
-echo ">>> Which Services are being used?"
-# While it *can* be answered in the Config console UI (given enough clicks), or using Cost Explorer (fewer clicks), 
-if [ "$OS_TYPE" == "macOS" ]; then  # it's on a Mac:
-   # For MacOS: https://stackoverflow.com/questions/63559669/get-first-date-of-current-month-in-macos-terminal
-   # And https://www.freebsd.org/cgi/man.cgi?date
-   MONTH_FIRST_DAY=$( date -v1d -v"$(date '+%m')"m '+%Y-%m-%d' )  # yields 09-01
-      # The -v1d is a time adjust flag to move to the first day of the month
-      # In -v"$(date '+%m')"m, we get the current month number using date '+%m' and use it to populate the month adjust field. So e.g. for Aug 2020, its set to -v8m
-      # The '+%F' prints the date in YYYY-MM-DD format. If not supported in your date version, use +%Y-%m-%d explicitly.
-      # To print all 12 months: for mon in {1..12}; do; date -v1d -v"$mon"m '+%F'; done
-   # ERROR: MONTH_LAST_DAY=$( date -v2d -v-1d '+%Y-%m-%d' )  # for 2021-09-30
-else
-   # Linux?
-   MONTH_FIRST_DAY=$(date "+%Y-%m-01" -d "-1 Month")
-fi
-   # This requires GNU date and is not portable (notably Mac / *BSD date is different)
-   aws ce get-cost-and-usage --time-period Start="$MONTH_FIRST_DAY",End=$(date --date="$(date +'%Y-%m-01') - 1 second" -I) \
-      --granularity MONTHLY --metrics UsageQuantity \
-      --group-by Type=DIMENSION,Key=SERVICE | jq '.ResultsByTime[].Groups[] | select(.Metrics.UsageQuantity.Amount > 0) | .Keys[0]'
-      # date "+%Y-%m-01" yields 2021-09-01, see https://www.cyberciti.biz/faq/linux-unix-formatting-dates-for-display/
-      # See https://stackoverflow.com/questions/27920201/how-can-i-get-the-1st-and-last-date-of-the-previous-month-in-a-bash-script/46897063
-
-   echo ">>> What is each service costing me (for the current month):"
-   aws ce get-cost-and-usage --time-period Start=$(date "+%Y-%m-01"),End=$(date --date="$(date +'%Y-%m-01') + 1 month  - 1 second" -I) \
-      --granularity MONTHLY --metrics USAGE_QUANTITY BLENDED_COST  \
-      --group-by Type=DIMENSION,Key=SERVICE | jq '[ .ResultsByTime[].Groups[] | select(.Metrics.BlendedCost.Amount > "0") | { (.Keys[0]): .Metrics.BlendedCost } ] | sort_by(.Amount) | add'
 
 # cat vpc.dat | jq  '.[0]'
 #'.Vpcs[] '
